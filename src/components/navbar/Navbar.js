1b6ba74a59433763bb082d3bc7e2ea2c49cc8503
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2023-01-16 11:36:45 +0000

    feat: navbar added

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import {React} from 'react'
+import './Navbar.css'
+
+export default function Navbar(){
+    return(
+        <>
+            <div className='navbar'> 
+                <img src='/img/retink-logo.jpeg' alt='retink logo'></img>
+            </div>
+        </>
+    )
+}
+
