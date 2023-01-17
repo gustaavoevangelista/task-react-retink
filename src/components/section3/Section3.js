48ea29c975aa21b7d71014d5d7525626f565a7c4
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2023-01-17 11:56:56 +0000

    feat: cards rendered dynamically

--- src/components/section3/Section3.js
+++ src/components/section3/Section3.js
@@ -1,57 +1,51 @@
 import './Section3.css'
 import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from 'react';
 
 
 export default function Section3() {
+    const [data, setData] = useState([])
+
+    const fetchJson = () => {
+        fetch('./data.json')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setData(data);
+
+            })
+    }
+
+    useEffect(() => {
+        fetchJson()
+    }, [])
+
     return (
         <>
             <div className='cards'>
+                {data.map((i, key) => {
+                    return (
+                        <Card key={key} className="card-bootstrap">
+                            <Card.Img variant="top" src={`/img/section3/${i.img}`} />
 
-                <Card className="card-bootstrap">
-                    <Card.Img variant="top" src="/img/section3/AI.svg" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    {i.title}
+                                </Card.Title>
 
-                    <Card.Body className='card-body'>
-                        <Card.Title className='card-title'>
-                            <span>MAKEAI-assisted</span>
-                            <span>Content Choices</span>
-                        </Card.Title>
+                                <Card.Text className='card-text'>
+                                    {i.text}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    )
 
-                        <Card.Text className='card-text'>
-                            Access and Order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="card-bootstrap">
-                    <Card.Img variant="top" src="/img/section3/AI.svg" />
-
-                    <Card.Body className='card-body'>
-                        <Card.Title className='card-title'>
-                            <span>MAKEAI-assisted</span>
-                            <span>Content Choices</span>
-                        </Card.Title>
-
-                        <Card.Text className='card-text'>
-                            Access and Order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="card-bootstrap">
-                    <Card.Img variant="top" src="/img/section3/AI.svg" />
-
-                    <Card.Body className='card-body'>
-                        <Card.Title className='card-title'>
-                            <span>MAKEAI-assisted</span>
-                            <span>Content Choices</span>
-                        </Card.Title>
-
-                        <Card.Text className='card-text'>
-                            Access and Order from an extensive catalogue in any language and on your budget's terms. If unsure, our AI guides you to the best.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                })
+                }
+                
             </div>
-
+            
         </>
     )
-
 }
