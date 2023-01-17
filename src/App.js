107d96950ba01f3efb2697cee3c0bc3c1091ba57
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2023-01-17 12:21:36 +0000

    feat: section4 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/navbar/Navbar';
 import Section1 from './components/section1/Section1';
 import Section2 from './components/section2/Section2';
 import Section3 from './components/section3/Section3';
+import Section4 from './components/section4/Section4';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Section1 />
         <Section2 />
         <Section3 />
+        <Section4 />
+
+
 
       </div>
     </>
---
+++ src/components/section4/Section4.js
@@ -0,0 +1,15 @@
+import './Section4.css'
+
+export default function Section4(){
+    return (
+        <>
+            <div className='text-section4'>
+                <span>Transform your Creation Process</span>
+                <p>With a new approach toordering content, you can now stop juggling multiple documents and meetings and start publishing content faster and on demand.</p>
+
+                <span>Activate your business growth with RetinkContent. </span>
+                <p>Save time and maintain your brand identity within your budget range or sign up for affordable plans and still access multiple content services like:</p>
+            </div>
+        </>
+    )
+}
